d4d53286f70e1351d3f0d2293d5843062905bedb
Alexandre Lapene <lapene@stanford.edu>
2018-11-14 16:24:23 -0800

    fix issues for merge

--- PVTPackage/source/MultiphaseSystem/BlackOilMultiphaseSystem.hpp
+++ PVTPackage/source/MultiphaseSystem/BlackOilMultiphaseSystem.hpp
@@ -6,28 +6,39 @@
 
 namespace PVTPackage
 {
-	class BlackOilMultiphaseSystem final: public MultiphaseSystem
+	class BlackOilMultiphaseSystem final : public MultiphaseSystem
 	{
 	public:
 
 		BlackOilMultiphaseSystem(std::vector<PHASE_TYPE> phase_types,
-														 std::vector<std::vector<double>> PVTO,
-														 std::vector<double> PVTW,
-														 std::vector<std::vector<double>> PVTG,
-														 std::vector<double> DENSITY,
-														 std::vector<double> MW);
+			std::vector<std::vector<double>> PVTO,
+			std::vector<double> PVTW,
+			std::vector<std::vector<double>> PVTG,
+			std::vector<double> DENSITY,
+			std::vector<double> MW);
 
+		BlackOilMultiphaseSystem(std::vector<PHASE_TYPE> phase_types,
+			std::vector<std::string> table_file_names,
+			std::vector<double> DENSITY,
+			std::vector<double> MW);
 
-    ~BlackOilMultiphaseSystem() override;
+		~BlackOilMultiphaseSystem() override;
 
 		//void Flash(double pressure, double temperature, std::vector<double> feed, PhaseSplitModelOutputVariables& out_variables) override;
 
 	protected:
 
+		void CreatePhases(std::vector<PHASE_TYPE> phase_types,
+			std::vector<std::vector<double>> PVTO,
+			std::vector<double> PVTW,
+			std::vector<std::vector<double>> PVTG,
+			std::vector<double> DENSITY,
+			std::vector<double> MW);
+
 		//Flash pointer
 		//BlackOilFlash* m_BlackOilFlash;
 
-		
+
 	};
 
 
